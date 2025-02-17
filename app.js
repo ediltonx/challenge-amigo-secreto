112b00d2eea76c7298d9349252eb312cb0f17668
ediltonx <ediltonx@github.com>
2025-02-17 11:05:59 +0000

    implementada funcao adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+let amigo ;
+
+let invalidos = [""," ", null, undefined,'',' '];
+
+
+function adicionarAmigo(amigo){
+let documento = document.querySelector(".input-name");
+amigo = documento.value;
+
+if(invalidos.includes(amigo) ) { alert('Digite um nome válido!'); }
+
+else {
+amigos.push(amigo);
+alert("O amigo " + amigo + " foi adicionado com sucesso!");
+documento.value = '';
+adicionarHTML('.name-list','li',amigo);
+
+}
+
+
+}
+
+
+function adicionarHTML(local, elementoTag, valor) {
+    let documento = document.querySelector(local);
+    let elemento = document.createElement(elementoTag);
+    elemento.innerHTML = valor;
+    documento.append(elemento);
+}
+
